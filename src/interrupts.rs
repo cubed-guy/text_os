356a3808e0256e6f90063f4fbe4516bd793d0d00
Cubed <47624167+cubed-guy@users.noreply.github.com>
2023-05-06 17:32:07 +0530

    Added proper scancode interpretation. End of post 7!

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -113,24 +113,28 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
 	use crate::print;
 
 	use x86_64::instructions::port::Port;
-	let mut port = Port::new(0x60);  // port of keyboard?
-	let scancode: u8 = unsafe { port.read() };
-
-	let key = match scancode {
-		0x02 => Some('1'),
-		0x03 => Some('2'),
-		0x04 => Some('3'),
-		0x05 => Some('4'),
-		0x06 => Some('5'),
-		0x07 => Some('6'),
-		0x08 => Some('7'),
-		0x09 => Some('8'),
-		0x0a => Some('9'),
-		0x0b => Some('0'),
-		_ => None,
+	use pc_keyboard::{
+		layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1
 	};
-	if let Some(key) = key {
-		print!("{}", key);
+	use spin::Mutex;
+
+	lazy_static! {
+		static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+			Mutex::new(Keyboard::new(
+				layouts::Us104Key, ScancodeSet1, HandleControl::Ignore
+			));
+	}
+
+	let mut port = Port::new(0x60);  // port of keyboard? port of keyboard.
+	let mut keyboard = KEYBOARD.lock();
+	let scancode: u8 = unsafe { port.read() };
+	if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+		if let Some(key) = keyboard.process_keyevent(key_event) {
+			match key {
+				DecodedKey::Unicode(character) => print!("{}", character),
+				DecodedKey::RawKey(key) => print!("{:?}", key),
+			}
+		}
 	}
 
 	// Notify the PIC (not CPU) to end the interrupt and become available again
