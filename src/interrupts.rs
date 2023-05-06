607817a421df62b4040741c8a4ead1053c825e0b
Cubed <47624167+cubed-guy@users.noreply.github.com>
2023-05-06 17:07:07 +0530

    Added manual scancode interpretation

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -111,7 +111,27 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
 	_stack_frame: InterruptStackFrame,
 ) {
 	use crate::print;
-	print!("k");
+
+	use x86_64::instructions::port::Port;
+	let mut port = Port::new(0x60);  // port of keyboard?
+	let scancode: u8 = unsafe { port.read() };
+
+	let key = match scancode {
+		0x02 => Some('1'),
+		0x03 => Some('2'),
+		0x04 => Some('3'),
+		0x05 => Some('4'),
+		0x06 => Some('5'),
+		0x07 => Some('6'),
+		0x08 => Some('7'),
+		0x09 => Some('8'),
+		0x0a => Some('9'),
+		0x0b => Some('0'),
+		_ => None,
+	};
+	if let Some(key) = key {
+		print!("{}", key);
+	}
 
 	// Notify the PIC (not CPU) to end the interrupt and become available again
 	// ----***  Don't forget to use the correct interrupt index!  ***----
